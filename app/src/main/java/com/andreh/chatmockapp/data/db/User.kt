6b77c6da56c2b761762ce14a1f9a7508e9c324d8
andreh111 <andrehabboud1@gmail.com>
2020-07-19 02:41:10 +0300

    Creating project structure and adding some files and gradle dependencies

---
+++ app/src/main/java/com/andreh/chatmockapp/data/db/User.kt
@@ -0,0 +1,12 @@
+package com.andreh.chatmockapp.data.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users_data_table")
+class User (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var name: String,
+    val photo: String
+)
---
+++ app/src/main/java/com/andreh/chatmockapp/ui/users/UsersActivity.kt
@@ -0,0 +1,12 @@
+package com.andreh.chatmockapp.ui.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.andreh.chatmockapp.R
+
+class UsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}
